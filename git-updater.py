6c2d6043e7f3e2e67f7f79ba05ee0210f4304d24
alex_termux <alex.deloverov@gmail.com>
2026-03-02 14:28:58 +0700

    MFDBA Update 14:28 02.03 Mod: git-updater.py

--- git-updater.py
+++ git-updater.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 import subprocess
 import sys
+import os
 from datetime import datetime
 
 def git(cmd):
     r = subprocess.run(['git'] + cmd.split(), capture_output=True, text=True)
     if r.returncode:
-        print(f"Git error: {r.stderr}")
         sys.exit(1)
     return r.stdout.strip()
 
@@ -16,31 +16,31 @@ def main():
         print('No changes')
         return
     
-    modified = []
-    new = []
-    for line in s.splitlines():
-        if line:
-            status = line[0]
-            filename = line[2:]
-            if status in 'MA':
-                modified.append(filename)
-            if status == '?':
-                new.append(filename)
+    m = []
+    n = []
+    for l in s.splitlines():
+        if l:
+            st = l[0]
+            f = l[2:]
+            if st in 'MA':
+                m.append(f)
+            if st == '?':
+                n.append(f)
     
     ts = datetime.now().strftime('%H:%M %d.%m')
     msg = 'MFDBA Update ' + ts
-    if modified:
-        msg += '\nMod: ' + ','.join(modified[:3])
-    if new:
-        msg += '\nNew: ' + ','.join(new[:2])
+    if m:
+        msg += '\nMod: ' + ','.join(m[:3])
+    if n:
+        msg += '\nNew: ' + ','.join(n[:2])
     
     print('Commit:\n' + msg)
     if input('Push y/n? ').lower() != 'y':
         return
     
-    subprocess.run(['git', 'add', '.'], check=True)
-    subprocess.run(['git', 'commit', '-m', msg], check=True)
-    subprocess.run(['git', 'push'], check=True)
+    os.system('git add .')
+    os.system('git commit -m "' + msg + '"')
+    os.system('git push')
     print('Done!')
 
 if __name__ == '__main__':
